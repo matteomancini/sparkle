b0de406f93afb83dd040326a2143dd6fda54f268
Oleksii Pohorelyi <oleksii.pohorelyi@gmail.com>
2021-05-27 16:44:57 +0300

    updates

--- src/pages/VenuePage/TemplateWrapper.tsx
+++ src/pages/VenuePage/TemplateWrapper.tsx
@@ -7,7 +7,6 @@ import { WithId } from "utils/id";
 
 import { ReactionsProvider } from "hooks/reactions";
 import { RelatedVenuesProvider } from "hooks/useRelatedVenues";
-// import { useShowHide } from "hooks/useShowHide";
 
 import { FriendShipPage } from "pages/FriendShipPage";
 
@@ -29,8 +28,6 @@ import { WithNavigationBar } from "components/organisms/WithNavigationBar";
 
 import { AnnouncementMessage } from "components/molecules/AnnouncementMessage";
 
-import { AnnouncementContainer } from "./AnnouncementContainer";
-
 export interface TemplateWrapperProps {
   venue: WithId<AnyVenue>;
 }
@@ -38,8 +35,6 @@ export interface TemplateWrapperProps {
 const TemplateWrapper: React.FC<TemplateWrapperProps> = ({ venue }) => {
   const history = useHistory();
   const match = useRouteMatch();
-
-  // const { isShown, show, hide } = useShowHide();
 
   let template;
   // @debt remove backButton from Navbar
@@ -151,9 +146,7 @@ const TemplateWrapper: React.FC<TemplateWrapperProps> = ({ venue }) => {
           fullscreen={fullscreen}
           hasBackButton={hasBackButton}
         >
-          <AnnouncementContainer isFullScreen={venue?.banner?.isFullScreen}>
-            <AnnouncementMessage banner={venue.banner} isCancel />
-          </AnnouncementContainer>
+          <AnnouncementMessage banner={venue.banner} isUser />
           {template}
           <ChatSidebar />
           <UserProfileModal venue={venue} />
